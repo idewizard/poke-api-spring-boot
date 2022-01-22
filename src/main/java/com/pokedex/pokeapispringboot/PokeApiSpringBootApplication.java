96574d73382fd9f194717bfe32c4a1f38e78ad10
Andiie <andre.sousa.tec@gmail.com>
2022-01-22 03:09:46 -0300

    Initial commit

---
+++ src/main/java/com/pokedex/pokeapispringboot/PokeApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.pokedex.pokeapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokedex/pokeapispringboot/PokeApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokedex.pokeapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
